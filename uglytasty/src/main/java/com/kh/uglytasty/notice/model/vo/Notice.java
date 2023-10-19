3f936eb78b861d9d6145f75e9ade91c86d0c3c8e
sy <lovesykim1@gmail.com>
2023-10-19 10:03:59 +0900

    fix : Notice vo 수정 - notice vo에 중복 필드 존재하여 수정하였습니다.

--- uglytasty/src/main/java/com/kh/uglytasty/notice/model/vo/Notice.java
+++ uglytasty/src/main/java/com/kh/uglytasty/notice/model/vo/Notice.java
@@ -20,9 +20,6 @@ public class Notice {
 	private String noticeContent;
 	private String status;
 	private String upfix;
-	private String noticeTitle;
-	private String noticeContent;
-	private String status;
 	private String upFix;
 
 
